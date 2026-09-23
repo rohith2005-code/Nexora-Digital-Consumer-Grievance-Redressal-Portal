345c2e4517b7d9bdd945118c50c960b480e5e253
rohith <rohith@gmail.com>
2026-09-23 08:12:38 +0530

    Fix build.sh permissions and add wsgi boot auto-seeding

--- grievance_core/wsgi.py
+++ grievance_core/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grievance_core.settings')
 
 application = get_wsgi_application()
+
+# Guarantee default accounts and migrations on server boot
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+    from complaints.views import ensure_default_accounts
+    ensure_default_accounts()
+except Exception as e:
+    print("Auto-seed error on WSGI boot:", e)
